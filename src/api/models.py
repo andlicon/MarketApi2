5392ccb52d4fde5c8d25f5fbd512af3e42b6a1cc
andlicon <andresjzabalac@hotmail.com>
2023-09-18 11:24:18 -0400

    ADD: product by order

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,93 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import enum
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    created_at = db.Column(db.DateTime , default=datetime.now)
+    updated_at = db.Column(db.DateTime , default=datetime.now, onupdate=datetime.now)
+    # relationship
+    orders_user = db.relationship('Order', backref='user')
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f'<User {self.id}>'
 
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
+        }
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    price = db.Column(db.Float, unique=False, nullable=False)
+    created_at = db.Column(db.DateTime , default=datetime.now)
+    updated_at = db.Column(db.DateTime , default=datetime.now, onupdate=datetime.now)
+    # relationship
+    product_order = db.relationship('ProductOrder', backref='product')
+
+    def __repr__(self):
+        return f'<Product {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "price": self.price,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
+        }
+
+class ProductOrder(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'))
+    amount = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f'<ProductList {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "product_id": self.product_id,
+            "order_id": self.order_id,
+        }
+
+
+class OrderStatus(enum.Enum):
+    ORDERED = 'ordered'
+    SALE = 'sale'
+    
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    status = db.Column(db.Enum(OrderStatus), nullable=False, default=OrderStatus.ORDERED)
+    created_at = db.Column(db.DateTime , default=datetime.now)
+    updated_at = db.Column(db.DateTime , default=datetime.now, onupdate=datetime.now)
+    # relationship
+    product_order = db.relationship('ProductOrder', backref='order')
+
+    def __repr__(self):
+        return f'<Order {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "product_order_id": self.product_order_id,
+            "amount": self.amount,
+            "status": self.status.value,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,17 +2,190 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Product, ProductOrder, Order
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+# get All users
+@api.route('/users', methods=['GET'])
+def get_all_user():
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+    user_list = User.query.all()
+    serialized = list(map(lambda user: user.serialize(), user_list))
 
-    return jsonify(response_body), 200
+    return jsonify(serialized), 200
+
+# add a new user
+@api.route('/users', methods=['POST'])
+def add_user():
+    if not request.is_json:
+        return jsonify({'msg': 'Body must be a JSON object'}), 400
+
+    body = request.get_json()
+    name = body.get('name')
+    email = body.get('email')
+    if None in [name, email]:
+        return jsonify({'msg': 'Wrong properties'}), 400
+
+    any_user = User.query.filter_by(email=email).one_or_none()
+    if any_user is not None:
+        return jsonify({'msg': 'Email already chosen'}), 400
+
+    user = User(name=name, email=email)
+
+    try:
+        db.session.add(user)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        print(e)
+        return jsonify({'msg': 'Some internal error'}), 500
+
+    return jsonify({'msg': 'User added'}), 202
+
+
+# get All products
+@api.route('/products', methods=['GET'])
+def get_all_products():
+
+    product_list = Product.query.all()
+    serialized = list(map(lambda product: product.serialize(), product_list))
+
+    return jsonify(serialized), 200
+
+# add a new product
+@api.route('/products', methods=['POST'])
+def add_product():
+    if not request.is_json:
+        return jsonify({'msg': 'Body must be a JSON object'}), 400
+
+    body = request.get_json()
+    name = body.get('name')
+    price = body.get('price')
+    if None in [name, price]:
+        return jsonify({'msg': 'Wrong properties'}), 400
+
+    any_product = Product.query.filter_by(name=name).one_or_none()
+    if any_product is not None:
+        return jsonify({'msg': 'There is a product already.'}), 400
+
+    product = Product(name=name, price=price)
+
+    try:
+        db.session.add(product)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        print(e)
+        return jsonify({'msg': 'Some internal error'}), 500
+
+    return jsonify({'msg': 'Product added'}), 202
+
+
+# add a new product
+@api.route('/products/<int:id>', methods=['DELETE'])
+def remove_product(id):
+    product = Product.query.filter_by(id=id).one_or_none()
+    if product is None:
+        return jsonify({'msg': 'Product not found'}), 404
+
+    try:
+        db.session.delete(product)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        print(e)
+        return jsonify({'msg': 'Some internal error'}), 500
+
+    return jsonify({'msg': 'Product deleted'}), 200
+
+
+# put a new product
+@api.route('/products/<int:id>', methods=['PUT'])
+def put_product(id):
+    if not request.is_json:
+        return jsonify({'msg': 'Body must be a JSON object'}), 400
+
+    product = Product.query.filter_by(id=id).one_or_none()
+    if product is None:
+        return jsonify({'msg': 'Product not found'}), 404
+
+    body = request.get_json()
+    name = body.get('name')
+    price = body.get('price')
+    if None in [name, price]:
+        return jsonify({'msg': 'Wrong properties'}), 400
+
+    product.name = name
+    product.price = price
+
+    try:
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        print(e)
+        return jsonify({'msg': 'Some internal error'}), 500
+
+    return jsonify({'msg': 'Product updated'}), 200
+
+
+# patch a new product
+@api.route('/products/<int:id>', methods=['PATCH'])
+def patch_product(id):
+    if not request.is_json:
+        return jsonify({'msg': 'Body must be a JSON object'}), 400
+
+    product = Product.query.filter_by(id=id).one_or_none()
+    if product is None:
+        return jsonify({'msg': 'Product not found'}), 404
+
+    body = request.get_json()
+    name = body.get('name')
+    price = body.get('price')
+
+    product.name = name if name is not None else product.name
+    product.price = price if price is not None else product.price
+
+    try:
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        print(e)
+        return jsonify({'msg': 'Some internal error'}), 500
+
+    return jsonify({'msg': 'Product updated'}), 200
+
+
+# all product user sale
+@api.route('/sales/user/<int:id>', methods=['GET'])
+def user_product_sale(id):
+    user = User.query.filter_by(id=id).one_or_none()
+    if user is None:
+        return jsonify({'msg': 'User not found'}), 404
+
+    product_sales = Product.query.filter_by(user_id=user.id, status=OrderStatus.SALE)
+    serialized = list(map(lambda product: product.serialize(), product_sales))
+
+    return jsonify(serialized), 200
+
+
+# all product by order
+@api.route('/order/<int:id>', methods=['GET'])
+def get_order(id):
+    order = Order.query.filter_by(id=id).one_or_none()
+    if order is None:
+        return jsonify({'msg': 'Order not found'}), 404
+
+    product_order_list = ProductOrder.query.filter_by(order_id=id)
+    serialized = []
+    for product_order in product_order_list:
+        obj = {
+            "amount": product_order.amount,
+            "product": product_order.product.serialize()
+        }
+        serialized.append(obj)
+
+
+    return jsonify(serialized), 200
